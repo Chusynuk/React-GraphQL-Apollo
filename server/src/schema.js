ac8dce6a7111e81b034bfe13c8ea6fd8b48f3745
eloi <recycled_loops@hotmail.com>
2020-10-27 15:22:34 +0100

    server refactor

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,51 @@
-const { gql } = require("apollo-server");
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String
+
+    # for use with the iOS tutorial
+    uploadProfileImage(file: Upload!): User
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
   type Launch {
     id: ID!
     site: String
@@ -18,35 +63,19 @@ const typeDefs = gql`
   type User {
     id: ID!
     email: String!
+    profileImage: String
     trips: [Launch]!
+    token: String
   }
 
   type Mission {
-      name: String
-      missionPatch(size: PatchSize): String
+    name: String
+    missionPatch(size: PatchSize): String
   }
 
   enum PatchSize {
-      SMALL
-      LARGE
-  }
-
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    me: User
-  }
-
-  type Mutation {
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-    login(email: String): String #login token
-  }
-
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launch: [Launch]
+    SMALL
+    LARGE
   }
 `;
 
